9cb60b00adb08394f5f234cf17701b15d15b47b3
Adrián Mijarra <adrian.mijarra@gmail.com>
2024-01-17 21:55:51 +0100

    fallo de rutas

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const indexRouter = require('./routes/index');
 
 const authRoutes = require('./routes/auth');
 
-const boardRoutes = require('./routes/post');
+const b = require('./routes/post');
 
 const app = express();
 
@@ -69,15 +69,6 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-
-  app.use(express.urlencoded({ extended: true }));
-
-  app.post('/saveProfileChanges', (req, res) => {
-    const { universityStudies, professionalPath, school, leisureAreas } = req.body;
-    // Aquí puedes hacer lo que necesites con los datos, como almacenarlos en una base de datos
-    console.log('Datos recibidos:', { universityStudies, professionalPath, school, leisureAreas });
-    res.redirect('/'); // Redirige a la página principal después de procesar los datos
-});
   
   res.locals.error = req.app.get('env') === 'development' ? err : {}; 
   // render the error page
